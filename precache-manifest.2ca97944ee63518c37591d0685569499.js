c3068c8be6f2fbccb32aec10afda90cde3c10839
Egor Gorbik <gorbik1966@mail.ru>
2020-03-23 22:02:59 +0300

    Updates

---
+++ precache-manifest.2ca97944ee63518c37591d0685569499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91554fb2405eac1bac5670416f134e82",
+    "url": "/house_demo/index.html"
+  },
+  {
+    "revision": "dca2da26c3b3cb36d5f3",
+    "url": "/house_demo/static/css/main.25a5f038.chunk.css"
+  },
+  {
+    "revision": "606cb684924702cd771d",
+    "url": "/house_demo/static/js/2.89f7779e.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/house_demo/static/js/2.89f7779e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca2da26c3b3cb36d5f3",
+    "url": "/house_demo/static/js/main.03b11032.chunk.js"
+  },
+  {
+    "revision": "10c69fe676a341d1ff63",
+    "url": "/house_demo/static/js/runtime-main.f95158a7.js"
+  }
+]);
